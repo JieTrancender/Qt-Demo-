6642c524ca14f36e2cce3d05bef2b217787f8020
JTrancender <jie-email@jie-trancender.org>
2016-11-08 20:53:47 +0800

    update qt_app

--- qt_app/mainwindow.cpp
+++ qt_app/mainwindow.cpp
@@ -1,6 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QDialog>
+#include <QMessageBox>
+#include <QPushButton>
+#include <QFileDialog>
+#include <QTextStream>
+#include <QLineEdit>
+#include <QLabel>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -10,12 +16,41 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->pushButton->setText("new windows");
     dlg = new QDialog(this);
 
-    QAction *openAction = new QAction(tr("&Open"), this);
+    /*QAction *openAction = new QAction(tr("&Open"), this);
     QIcon icon(":/images/li.jpg");
     openAction->setIcon(icon);
 
     openAction->setShortcut(QKeySequence(tr("Ctrl+O")));
-    ui->menu_F->addAction(openAction);
+    ui->menu_F->addAction(openAction);*/
+
+    isUntitled = true;
+    curFile = tr("UnnamedFile.txt");
+    setWindowTitle(curFile);
+
+    findDlg = new QDialog(this);
+    findDlg->setWindowTitle(tr("查找"));
+    findLineEdit = new QLineEdit(findDlg);
+    QPushButton *btn = new QPushButton(tr("Search Next"), findDlg);
+    QVBoxLayout *layout = new QVBoxLayout(findDlg);
+    layout->addWidget(findLineEdit);
+    layout->addWidget(btn);
+    //connect(btn, SIGNAL(clicked(bool), this, SLOT(showFindText())));
+    connect(btn, SIGNAL(clicked()),this,SLOT(showFindText()));
+
+    //ui->statusBar->showMessage("Welcome to JieTrancender", 8000);  //显示8s钟
+    statusLabel = new QLabel;
+    statusLabel->setMinimumSize(150, 20);  //设置label大小
+    statusLabel->setFrameShape(QFrame::WinPanel);  //设置标签形状
+    statusLabel->setFrameShadow(QFrame::Sunken);  //设置标签阴影
+    ui->statusBar->addWidget(statusLabel);
+    statusLabel->setText("Welcome to jie-email@jie-trancender.org");
+
+    QLabel *permanent = new QLabel(this);
+    permanent->setFrameStyle(QFrame::Box | QFrame::Sunken);
+    permanent->setText(tr("<a href = \"http://jie-trancender.org\">JTrancender</a>"));
+    permanent->setTextFormat(Qt::RichText);
+    permanent->setOpenExternalLinks(true);
+    ui->statusBar->addPermanentWidget(permanent);
 }
 
 MainWindow::~MainWindow()
@@ -23,8 +58,201 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+//判断文档是否需要保存，如果已经保存完了，则新建文档，并初始化
+void MainWindow::newFile()
+{
+    if (maybeSave())
+    {
+        isUntitled = true;
+        curFile = tr("UnnamedFile.txt");
+        setWindowTitle(curFile);
+        ui->textEdit->clear();
+        ui->textEdit->setVisible(true);
+    }
+}
+
+//判断文档是否需要保存
+bool MainWindow::maybeSave()
+{
+    if (ui->textEdit->document()->isModified())
+    {
+        QMessageBox box;
+        box.setWindowTitle("Warning");
+        box.setIcon(QMessageBox::Warning);
+        box.setText(curFile + " hasn't saved, is save?");
+        QPushButton *yesBtn = box.addButton("Yes(&Y)", QMessageBox::YesRole);
+        //QPushButton *noBtn = box.addButton("No(&N)", QMessageBox::NoRole);
+        QPushButton *cancelBtn = box.addButton("Cancel", QMessageBox::RejectRole);
+        box.exec();
+
+        if (box.clickedButton() == yesBtn)
+        {
+            return save();
+        }
+        else if (box.clickedButton() == cancelBtn)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool MainWindow::save()
+{
+    if (isUntitled)
+    {
+        return saveAs();
+    }
+    else
+    {
+        return saveFile(curFile);
+    }
+}
+
+bool MainWindow::saveAs()
+{
+    QString fileName = QFileDialog::getSaveFileName(this, "Save As", curFile);
+
+    if (fileName.isEmpty())
+    {
+        return false;
+    }
+    return saveFile(fileName);
+}
+
+bool MainWindow::saveFile(const QString &fileName)
+{
+    QFile file(fileName);
+
+    if (!file.open(QFile::WriteOnly | QFile::Text))
+    {
+        QMessageBox::warning(this, "Multi file editor", tr("can't write file %1:\n %2").arg(fileName).arg(file.errorString()));
+
+        return false;
+    }
+
+    QTextStream out(&file);
+    QApplication::setOverrideCursor(Qt::WaitCursor);  //鼠标指针变为等待状态
+    out << ui->textEdit->toPlainText();
+
+    QApplication::restoreOverrideCursor();  //鼠标指针恢复原来的状态
+    isUntitled = false;
+
+    curFile = QFileInfo(fileName).canonicalFilePath();  //获得文件标准路径
+    setWindowTitle(curFile);
+    return true;
+}
+
+bool MainWindow::loadFile(const QString &fileName)
+{
+    QFile file(fileName);
+    if (!file.open(QFile::ReadOnly | QFile::Text))
+    {
+        QMessageBox::warning(this, tr("多文档编辑器"), tr("无法读取文件 %1:\n%2").arg(fileName).arg(file.errorString()));
+        return false;
+    }
+
+    QTextStream in(&file);
+    QApplication::setOverrideCursor(Qt::WaitCursor);
+    ui->textEdit->setPlainText(in.readAll());
+    QApplication::restoreOverrideCursor();
+
+    curFile = QFileInfo(fileName).canonicalFilePath();
+    setWindowTitle(curFile);
+    return true;
+}
+
+void MainWindow::closeEvent(QCloseEvent *event)
+{
+    if (maybeSave())
+    {
+        event->accept();
+    }
+    else
+    {
+        event->ignore();
+    }
+}
+
 void MainWindow::on_pushButton_clicked()
 {
     //dlg = new QDialog(this);
     dlg->show();
+}
+
+void MainWindow::on_actionNew_N_triggered()
+{
+    newFile();
+}
+
+void MainWindow::on_actionBaocun_triggered()
+{
+    save();
+}
+
+void MainWindow::on_actionLin_triggered()
+{
+    saveAs();
+}
+
+void MainWindow::on_action_open_file_triggered()
+{
+    if (maybeSave())
+    {
+        QString fileName = QFileDialog::getOpenFileName(this);
+
+        if (!fileName.isEmpty())
+        {
+            loadFile(fileName);
+            ui->textEdit->setVisible(true);
+        }
+    }
+}
+
+void MainWindow::on_action_close_file_triggered()
+{
+    if (maybeSave())
+    {
+        ui->textEdit->setVisible(false);
+    }
+}
+
+void MainWindow::on_action_exit_triggered()
+{
+    on_action_close_file_triggered();
+    qApp->quit();
+}
+
+void MainWindow::on_action_revoke_triggered()
+{
+    ui->textEdit->undo();
+}
+
+void MainWindow::on_action_cut_triggered()
+{
+    ui->textEdit->cut();
+}
+
+void MainWindow::on_action_copy_triggered()
+{
+    ui->textEdit->copy();
+}
+
+void MainWindow::on_action_paste_triggered()
+{
+    ui->textEdit->paste();
+}
+
+void MainWindow::showFindText()
+{
+    QString str = findLineEdit->text();
+    if (!ui->textEdit->find(str, QTextDocument::FindBackward))
+    {
+        QMessageBox::warning(this, tr("查找"), tr("找不到%1").arg(str));
+    }
+}
+
+void MainWindow::on_actionFind_triggered()
+{
+    findDlg->show();
 }
--- qt_app/mainwindow.h
+++ qt_app/mainwindow.h
@@ -3,6 +3,11 @@
 
 #include <QMainWindow>
 #include <QDialog>
+#include <QCloseEvent>
+
+class QLineEdit;
+class QDialog;
+class QLabel;
 
 namespace Ui {
 class MainWindow;
@@ -16,12 +21,51 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+    void newFile();
+    bool maybeSave();
+    bool save();
+    bool saveAs();
+    bool saveFile(const QString &fileName);
+    bool loadFile(const QString &fileName);
+
 private slots:
     void on_pushButton_clicked();
+
+    void on_actionNew_N_triggered();
+
+    void on_actionBaocun_triggered();
+
+    void on_actionLin_triggered();
+
+    void on_action_open_file_triggered();
+
+    void on_action_close_file_triggered();
+
+    void on_action_exit_triggered();
+
+    void on_action_revoke_triggered();
+
+    void on_action_cut_triggered();
+
+    void on_action_copy_triggered();
+
+    void on_action_paste_triggered();
+
+    void showFindText();
+
+    void on_actionFind_triggered();
+
+protected:
+    void closeEvent(QCloseEvent *event);
 
 private:
     Ui::MainWindow *ui;
     QDialog *dlg;
+    bool isUntitled;  //为真表明没有保存过，否则已经被保存了
+    QString curFile;  //保存当前文件的路径
+    QLineEdit *findLineEdit;
+    QDialog *findDlg;
+    QLabel *statusLabel;
 };
 
 #endif // MAINWINDOW_H
